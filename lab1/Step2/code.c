f0cd0388c163dff44880e575033a279ce3384c3c
andreasbakke <andreas.s.bakke@gmail.com>
2022-11-09 16:28:57 +0100

    added first 2 labs

---
+++ lab1/Step2/code.c
@@ -0,0 +1,168 @@
+
+
+
+/*system spec
+ two task
+ counter
+ two alarms
+ set counter to interrupt (low level ISR1) then generate event for other task
+
+
+ *Some lines are omitted here that will be already present
+ *
+ */
+
+//libraries
+#include "ee.h"
+#include "ee_irq.h"
+
+//Low level hardware specific definitions
+#define __USE_LEDS__
+#define __USE_BUTTONS__
+#include "board/axiom_mpc5674fxmb/inc/ee_board.h"
+
+volatile unsigned int ERROR_FLAG = 0;
+
+//add task by declaring
+DeclareTask( Task1);
+DeclareTask( Task2);
+//add events by declaring
+DeclareEvent( TimerEvent1);
+DeclareEvent( TimerEvent2);
+
+/* some prototypes... */
+//Advice only: define all prototypes of functions created in the project
+//two functions and two global variables
+//two function
+void mydelay(unsigned long int del); //to form a loop creating a delay
+void led_blink(unsigned char theled); //to change led configuration: blinking or not blinking
+
+//global variables to set global status of led
+volatile int led_status = 0; //command compiler to not optimize the variable
+volatile int myErrorCounter; //to count number of errors to check the error hook working
+/**/
+
+/* just a dummy delay */
+void mydelay(unsigned long int del) { //for loop stupid. busy wait
+	volatile long int i; //compiler may remove because loop and no return of value  so volatile.
+	for (i = 0; i < del; i++) {
+		//empty
+	}
+}
+/**/
+
+/* Added for a finest debugger (run-time) tuning */
+int tunable_delay = 50000;
+/**/
+
+/* sets and resets a led configuration passed as parameter, leaving the other
+ * bits unchanged...
+ */
+void led_blink(unsigned char theled) { //Exam: not asked to write this function just implement?
+	/*DisableAllInterrupts(); //process gets control and work without interruption. Scheduler stopped. if single core.
+	led_status |= theled; //ledStatus = ledStatus bitwiseor theled unsafe but in C works. The desired led set to 1.
+	EE_leds(led_status); //targetBoard EE,
+	EnableAllInterrupts(); //disable enable similar to synchronization lock and unlock for the system
+	mydelay((long int) tunable_delay);*/
+
+	DisableAllInterrupts();
+	led_status ^= theled; //flip the desired led status. bitwise AND NOT
+	EE_leds(led_status);
+	EnableAllInterrupts();
+
+}
+
+//Exam: asked to fill the function
+/* BUTTON_0 interrupts activate Task2. */
+static void Buttons_Interrupt(void) { //ISR1 not needed in OIL but still used service routine hardware specific.
+//	set alarmtask2 :> run task2*/ //Exam comment to be filled with code
+	led_blink(0x01);
+	EE_buttons_clear_ISRflag(BUTTON_0); //exam: filled.
+}
+
+static void Counter_Interrupt(void) {
+	CounterTick(myCounter);
+
+	EE_e200z7_setup_decrementer(2000); //target board specific
+}
+
+void ErrorHook(StatusType Error) { //executed when system catches error
+	myErrorCounter++; //increment global counter //just in case although not executed due to infinite loop
+	ERROR_FLAG = Error; // global variable set to error
+	led_blink(0xFF);
+}
+
+/*
+ * The StartupHook in this case is used to initialize the Button and timer
+ * interrupts
+ */
+void StartupHook(void) { //runs at boot . can be done at initial part in main. hook more elegant
+	EE_buttons_init(BUTTON_0, 3); //low level function. implementation board specific
+
+	//in datasheet of ERIKA
+	EE_e200z7_register_ISR(46 + 16, Buttons_Interrupt, 1); //initialize interrupts
+	EE_e200z7_register_ISR(10, Counter_Interrupt, 0); //in datasheet of Erika
+	EE_e200z7_setup_decrementer(2000); ////in datasheet of Erika
+
+}
+
+TASK( Task1) {
+	//led_blink(0b10000000); //Basic tasks
+	//TerminateTask();	//Basic Tasks
+
+
+	//Extended Task
+	EventMaskType mask1;
+
+	while(1){
+		WaitEvent(TimerEvent1); //wait any event? No. why specify event? Ans: event we want to wait. more than one event with bitwise |
+				{
+					GetEvent(Task1, &mask1); //to check which event received
+					if (mask1 & TimerEvent1) {
+						led_blink(0b00001000); //blink led 3
+						ClearEvent(TimerEvent1);
+					}
+				}
+	};
+	TerminateTask(); //safety
+}
+
+TASK( Task2) {
+	//led_blink(0b10000000); //Basic tasks
+	//TerminateTask();	//Basic Tasks
+
+
+	//Extended Task
+	EventMaskType mask2;
+
+	while(1){
+		WaitEvent(TimerEvent2); //wait any event? No. why specify event? Ans: event we want to wait. more than one event with bitwise |
+				{
+					GetEvent(Task2, &mask2); //to check which event received
+					if (mask2 & TimerEvent2) {
+						led_blink(0b10000000); //blink led 7
+						ClearEvent(TimerEvent2);
+					}
+				}
+	};
+	TerminateTask(); //safety
+}
+
+int main(void) {
+	EE_leds_init();
+	StartOS(OSDEFAULTAPPMODE);
+	EE_e200z7_enableIRQ();
+
+	for (;;) {
+	}
+	return 0;
+
+}
+
+//some errors are false flags
+
+
+
+
+
+
---
+++ lab1/step4/Debug/eecfg.h
@@ -0,0 +1,158 @@
+#ifndef RTDH_EECFG_H
+#define RTDH_EECFG_H
+
+
+#define RTDRUID_CONFIGURATOR_NUMBER 1278
+
+
+
+/***************************************************************************
+ *
+ * Common defines ( CPU 0 )
+ *
+ **************************************************************************/
+
+    /* TASK definition */
+    #define EE_MAX_TASK 2
+    #define SenderTask 0
+    #define Task2 1
+
+    /* MUTEX definition */
+    #define EE_MAX_RESOURCE 1U
+    #define RES_SCHEDULER 0U
+
+    /* EVENT definition */
+    #define TimerEvent1 0x1U
+    #define msgIn 0x2U
+
+    /* ALARM definition */
+    #define EE_MAX_ALARM 1U
+    #define AlarmTask1 0U
+
+    /* SCHEDULING TABLE definition */
+    #define EE_MAX_SCHEDULETABLE 0U
+
+    /* COUNTER OBJECTS definition */
+    #define EE_MAX_COUNTER_OBJECTS (EE_MAX_ALARM + EE_MAX_SCHEDULETABLE)
+
+    /* COUNTER definition */
+    #define EE_MAX_COUNTER 1U
+    #define myCounter 0U
+
+    /* APPMODE definition */
+    #define EE_MAX_APPMODE 1U
+
+    /* CPUs */
+    #define EE_MAX_CPU 1
+    #define EE_CURRENTCPU 0
+
+    /* Number of isr 2 */
+    #define EE_MAX_ISR2   0
+    #define EE_MAX_ISR_ID 0
+
+#ifndef __DISABLE_EEOPT_DEFINES__
+
+
+/***************************************************************************
+ *
+ * User options
+ *
+ **************************************************************************/
+#define __USE_LEDS__
+#define __USE_BUTTONS__
+#define __E200ZX_EXECUTE_FROM_RAM__
+#define EE_ISR_DYNAMIC_TABLE
+#define EE_LAUTERBACH_DEMO_SIM
+
+
+/***************************************************************************
+ *
+ * Automatic options
+ *
+ **************************************************************************/
+#define __RTD_LINUX__
+#define __PPCE200ZX__
+#define __MPC5674F__
+#define __PPCE200Z7__
+#define EE_GNU__
+#define __OO_ORTI_LASTERROR__
+#define __OO_ORTI_SERVICETRACE__
+#define __OO_ORTI_PRIORITY__
+#define __OO_ORTI_RES_LOCKER_TASK__
+#define __OO_ORTI_RES_ISLOCKED__
+#define __OO_ORTI_STACK__
+#define __OO_ORTI_ALARMTIME__
+#define __OO_ORTI_RUNNINGISR2__
+#define __COM_CCCA__
+#define __USE_STARTCOM_EXTENSION__
+#define __OO_ECC2__
+#define __OO_EXTENDED_STATUS__
+#define __OO_HAS_STARTUPHOOK__
+#define __OO_HAS_ERRORHOOK__
+#define __IRQ_STACK_NEEDED__
+#define __MULTI__
+#define __OO_HAS_USERESSCHEDULER__
+#define __OO_AUTOSTART_TASK__
+#define __OO_AUTOSTART_ALARM__
+#define __ALLOW_NESTED_IRQ__
+
+#endif
+
+
+
+/***************************************************************************
+ *
+ * COM application modes
+ *
+ **************************************************************************/
+#define EE_COM_N_MODE 1U
+
+#define LAB1_APPMODE 0U
+
+
+
+/***************************************************************************
+ *
+ * Messages
+ *
+ **************************************************************************/
+#define EE_COM_N_MSG 2U
+
+#define msgSendCount 0U
+#define msgReceiveCount 1U
+    /* System stack size */
+    #define EE_SYS_STACK_SIZE     4096
+
+
+
+/***************************************************************************
+ *
+ * Counter defines
+ *
+ **************************************************************************/
+#define OSMAXALLOWEDVALUE_myCounter 65535U
+#define OSTICKSPERBASE_myCounter    1U
+#define OSMINCYCLE_myCounter        2U
+
+
+
+/***************************************************************************
+ *
+ * Vector size defines
+ *
+ **************************************************************************/
+    #define EE_ACTION_ROM_SIZE 1
+    #define EE_ALARM_ROM_SIZE 1
+    #define EE_COUNTER_OBJECTS_ROM_SIZE 1
+    #define EE_COUNTER_ROM_SIZE 1
+    #define EE_E200Z7_SYSTEM_TOS_SIZE 3
+    #define EE_OO_AUTOSTART_ALARM_MODE_OSDEFAULTAPPMODE_SIZE 1
+    #define EE_OO_AUTOSTART_TASK_MODE_OSDEFAULTAPPMODE_SIZE 1
+    #define EE_RQ_PAIRS_NEXT_SIZE 2
+    #define EE_RQ_PAIRS_TID_SIZE 2
+    #define EE_RQ_QUEUES_HEAD_SIZE 16
+    #define EE_RQ_QUEUES_TAIL_SIZE 16
+
+
+#endif
+
